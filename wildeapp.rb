8fe20d51b2044b50cb6eb02d32a63a7cdc116057
jonnorstrom <jon.norstrom@gmail.com>
2016-03-14 19:47:09 -0500

    Change all titles

--- wildeapp.rb
+++ wildeapp.rb
@@ -26,29 +26,9 @@ post '/send_claims_mail' do
   redirect '/thank_you.html'
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 post '/send_request_mail' do
-  mailer = Postmark::ApiClient.new('a3410648-e7b1-4011-8827-183da0a3750d')
+  emailer = Postmark::ApiClient.new('a3410648-e7b1-4011-8827-183da0a3750d')
+  textmailer = Postmark::ApiClient.new('a3410648-e7b1-4011-8827-183da0a3750d')
       checked_boxes = []
       params.each do |key, value|
       if value == "on"
@@ -56,12 +36,17 @@ post '/send_request_mail' do
       end
 
     end
-  mailer.deliver(
+  emailer.deliver(
       from: "quotes@wildecorpinsurance.com",
-      to: ["jon.norstrom@gmail.com", "3307147018@vtext.com"],
+      to: "jon.norstrom@gmail.com",
       subject: "(blank) Insurance Quote Requested by #{params["first_name"]}",
       text_body: "#{params["first_name"]} #{params["last_name"]} has requested a quote for #{params["personal_or_commercial"]} insurance. They can be reached by phone at: #{params["phone_number"]} or by email at: '#{params["email"]}' They are interested in #{checked_boxes.join(", ")} insurance types. They left you this message:\n\n - #{params["comments"]}\n\n \n\n Thank you! Please get back to them soon."
   )
 
+  textmailer.deliver(
+      from: "claims@wildecorpinsurance.com",
+      to: "3305925150@vtext.com",
+      text_body: "#{params["first_name"]} #{params["last_name"]} has requested a #{params["personal_or_commercial"]} quote. Phone: #{params["phone_number"]} Email: #{params["email"]}. See your email for more info."
+  )
   redirect '/thank_you.html'
 end
